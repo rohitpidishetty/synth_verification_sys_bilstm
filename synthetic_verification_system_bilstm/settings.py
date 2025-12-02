69b0611b18869c20405f90be548125c7ecff463a
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-12-02 07:07:16 -0600

    Add CORS settings to Django configuration
    Added CORS support for specified origins.

--- synthetic_verification_system_bilstm/settings.py
+++ synthetic_verification_system_bilstm/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+     "corsheaders", 
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000",
+    "https://yaari-jud.web.app",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -134,3 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 BASE_URL = "https://yaari-dgawh2beexaycmg9.canadacentral-01.azurewebsites.net/test/"
+
